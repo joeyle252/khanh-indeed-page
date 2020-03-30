039055c54fa58f431da79b0a7141a277972eb8a8
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-31 00:28:08 +0700

    setup new project and implement routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState ,useEffect } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import CompanyPage from './pages/CompanyPage';
+import Candidates from './pages/Candidates';
+import NavBar from './pages/NavBar';
+
 
 function App() {
+
+  useEffect(() => {
+    const fetchCandidates = (async () => {
+      const resp = await fetch('http://localhost:3001/candidates');
+      const respJson = await resp.json();
+      console.log('khanh', respJson);
+      console.log('resp', resp)
+      fetchCandidates();
+    }
+    )
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/company">
+            <CompanyPage />
+          </Route>
+          <Route path="/candidates">
+            <Candidates />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/CompanyPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CompanyPage() {
+    return (
+        <div>
+            Company page
+        </div>
+    )
+}
